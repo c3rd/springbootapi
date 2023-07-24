fc414b4432a6ee3588371c2ac33af57ccf4e0387
Celio Maroja <c3rd.dev@gmail.com>
2023-07-24 13:56:24 -0300

    initial commit
    Signed-off-by: Celio Maroja <c3rd.dev@gmail.com>

---
+++ src/main/java/dev/c3rd/supercart/SupercartApplication.java
@@ -0,0 +1,13 @@
+package dev.c3rd.supercart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupercartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupercartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/c3rd/supercart/SupercartApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.c3rd.supercart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupercartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
